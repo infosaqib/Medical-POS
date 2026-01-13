dcba3b086faa19cdd8663c6e69d5a6bd63dbc47e
Saqib <hafizsaqi555@gmail.com>
2026-01-13 22:13:43 +0500

    feat: enhance bootstrap process with graph publishing options

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+   const shouldPublishGraph = process.env.PUBLISH_GRAPH === "true";
+ const app = await NestFactory.create(AppModule, {
+    snapshot: true,
+    preview: shouldPublishGraph,
+  });
 
   const config = new DocumentBuilder()
     .setTitle('NEST example')
@@ -15,6 +19,16 @@ async function bootstrap() {
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
 
-  await app.listen(process.env.PORT ?? 3000);
+  if (shouldPublishGraph) {
+    await app.init();
+
+    const publishOptions = { ... } // NOTE: this options object will vary depending on the CI/CD provider you're using
+    const graphPublisher = new GraphPublisher(app);
+    await graphPublisher.publish(publishOptions);
+
+    await app.close();
+  } else {
+    await app.listen(process.env.PORT ?? 3000);
+  }
 }
 bootstrap();
